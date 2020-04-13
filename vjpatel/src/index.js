b3d89fb4182e2e66697c1a8ab57a3ae1c73ccb1d
VJ Patel <vj@vjpatel.me>
2020-04-13 22:42:57 +0000

    inline fonts

--- vjpatel/src/index.js
+++ vjpatel/src/index.js
@@ -9,6 +9,10 @@ import dayjs from 'dayjs';
 var relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
+var Turbolinks = require("turbolinks");
+Turbolinks.start();
+Turbolinks.setProgressBarDelay(1);
+
 import jQuery from 'jquery/src/core';
 // import 'jquery/src/selector';
 // import 'jquery/src/traversing';
