1d0663e9eb3f13fd11c946fe55196098f917fa36
VJ Patel <VJftw@users.noreply.github.com>
2020-11-30 21:36:33 +0000

    add trailingSlash export

--- website/nextjs/next.config.js
+++ website/nextjs/next.config.js
@@ -1,7 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+const nextConfig = {
+  trailingSlash: true,
+};
+
 module.exports = withPlugins([
   [optimizedImages, {
   }],
-]);
+], nextConfig);
