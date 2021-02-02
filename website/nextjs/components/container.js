86d117827a9fc245d52b83c7bb22e6b97ec7659d
VJ Patel <vj@vjpatel.me>
2021-02-02 00:03:22 +0000

    consistent gutters

--- website/nextjs/components/container.js
+++ website/nextjs/components/container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto px-5 flex-grow overflow-x-auto">{children}</div>
+  return <div className="container mx-auto px-5 flex-grow overflow-x-auto justify-between max-w-7xl">{children}</div>
 }
